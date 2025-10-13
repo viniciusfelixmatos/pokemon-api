3c8f445d3b1263b5dc41d8269e0f46b24e331014
Vinicius Matos <163915966+viniciusfelixmatos@users.noreply.github.com>
2025-10-13 08:45:13 -0300

    feat: Melhorias no HTML e CSS, e estruturação da lógica em JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,49 @@
-const url = "https://pokeapi.co/api/v2/pokemon/1/"
-const templateCard = $(".pokedex-body__cards-template")
+const url = "https://pokeapi.co/api/v2/pokemon?limit=20";
+const templateCard = document.getElementById('pokedex-template');
+const container = document.querySelector('.pokedex-body__cards-container');
 
 async function getInfoPoke() {
     const response = await fetch(url);
     const data = await response.json();
-    console.log(data);
-    console.log(response);
 
-    data.species.map((card) => {
-        const name = document.createElement("p");
-        name.innerText = card.name;
+    data.results.forEach(async (pokemon) => {
+        // Faz a requisição individual
+        const pokeResponse = await fetch(pokemon.url);
+        const pokeData = await pokeResponse.json();
 
-        templateCard.appendChild(name);
+        // Clona o template
+        const card = templateCard.cloneNode(true);
+        card.style.display = "flex";
+        card.classList.remove("template");
+        card.classList.add("pokedex-card");
+        card.removeAttribute("id");
 
-    })
+        // Formatar ID
+        const id = pokemon.url.split("/")[6];
+        const formattedId = `#${id.toString().padStart(3, "0")}`;
 
+        // Preenche os dados
+        card.querySelector(".pokemon-img").src = pokeData.sprites.front_default;
+        card.querySelector(".pokemon-span__cards-id").textContent = formattedId;
+        card.querySelector(".pokemon-span__cards-name").textContent = pokeData.name;
+
+        // Tipos
+        const typeContainer = card.querySelector(".pokedex-body__cards-type");
+        typeContainer.innerHTML = "";
+
+        pokeData.types.forEach((t) => {
+            const typeDiv = document.createElement("div");
+            typeDiv.classList.add("pokedex-body__cards-especie");
+            typeDiv.innerHTML = `
+                <img src="img/${t.type.name}-type-icon.png" alt="${t.type.name}">
+                <span>${t.type.name}</span>
+            `;
+            typeContainer.appendChild(typeDiv);
+        });
+
+        // Adiciona o card ao container
+        container.appendChild(card);
+    });
 }
 
 getInfoPoke();
